64a6c361745a48c8e05ca9f302f156aec656fd88
Johannson17 <fedejohannson0@gmail.com>
2023-12-29 21:52:48 -0300

    new file:   .dockerignore 	new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   Contacto/__init__.py 	new file:   Contacto/admin.py 	new file:   Contacto/apps.py 	new file:   Contacto/forms.py 	new file:   Contacto/migrations/0001_initial.py 	new file:   Contacto/migrations/0002_alter_contact_numero.py 	new file:   Contacto/migrations/__init__.py 	new file:   Contacto/models.py 	new file:   Contacto/templates/contacts.html 	new file:   Contacto/tests.py 	new file:   Contacto/urls.py 	new file:   Contacto/views.py 	new file:   Dockerfile 	new file:   Mysite/__init__.py 	new file:   Mysite/asgi.py 	new file:   Mysite/settings.py 	new file:   Mysite/urls.py 	new file:   Mysite/wsgi.py 	new file:   Portafolio/__init__.py 	new file:   Portafolio/admin.py 	new file:   Portafolio/apps.py 	new file:   Portafolio/migrations/0001_initial.py 	new file:   Portafolio/migrations/__init__.py 	new file:   Portafolio/models.py 	new file:   Portafolio/static/contact.css 	new file:   Portafolio/static/footer.css 	new file:   Portafolio/static/main.css 	new file:   Portafolio/static/portfolio/Curriculum_Johannson.pdf 	new file:   Portafolio/static/portfolio/Fondo01.jpg 	new file:   Portafolio/templates/layout.html 	new file:   Portafolio/templates/partials/footer.html 	new file:   Portafolio/templates/partials/navbar.html 	new file:   Portafolio/templates/projects.html 	new file:   Portafolio/tests.py 	new file:   Portafolio/urls.py 	new file:   Portafolio/views.py 	new file:   build.sh 	new file:   fly.toml 	new file:   inicio/__init__.py 	new file:   inicio/admin.py 	new file:   inicio/apps.py 	new file:   inicio/migrations/0001_initial.py 	new file:   inicio/migrations/__init__.py 	new file:   inicio/models.py 	new file:   inicio/templates/me.html 	new file:   inicio/tests.py 	new file:   inicio/urls.py 	new file:   inicio/views.py 	new file:   manage.py 	new file:   media/static/inicio/Imagen_de_WhatsApp_2023-07-06_a_las_13.37.22.jpg 	new file:   media/static/inicio/fotito2.jpeg 	new file:   media/static/portfolio/Captura_de_pantalla_2023-06-23_163613.png 	new file:   media/static/portfolio/Imagen_de_WhatsApp_2023-08-16_a_las_14.37.41.jpg 	new file:   media/static/portfolio/Imagen_de_WhatsApp_2023-08-16_a_las_14.41.43.jpg 	new file:   requirements.txt

---
+++ inicio/models.py
@@ -0,0 +1,27 @@
+from django.db import models 
+from django.db.models.fields import CharField, URLField, TextField, DateField 
+from django.db.models.fields.files import ImageField
+import datetime 
+
+def upload_to_static(instance, filename):
+    return f'static/inicio/{filename}'
+
+class info(models.Model):
+    title = CharField(max_length=50)
+    sub_title = CharField(max_length=100)
+    description = TextField()
+    image = ImageField(upload_to=upload_to_static)
+
+class degree(models.Model):
+    title = CharField(max_length=50)
+    sub_title = CharField(max_length=100)
+    description = TextField()
+    date1 = DateField()
+    date = DateField(default=datetime.date.today)
+
+class experience(models.Model):
+    title = CharField(max_length=100)
+    description = TextField()
+    puesto = CharField(max_length=100)
+    date1 = DateField()
+    date = DateField(default=datetime.date.today)
